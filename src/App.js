d90d017eed3618b6f73efee761b61c06c47a7ce2
chaitu <chaitu@gxtechs.com>
2022-03-21 10:08:56 +0000

    t

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
+import Projects from './components/Projects'
 import './App.css'
 
-//This is the list (static data) used in the application. You can move it to any component if needed.
-
-const categoriesList = [
-  {id: 'ALL', displayText: 'All'},
-  {id: 'STATIC', displayText: 'Static'},
-  {id: 'RESPONSIVE', displayText: 'Responsive'},
-  {id: 'DYNAMIC', displayText: 'Dynamic'},
-  {id: 'REACT', displayText: 'React'},
-]
-
-// Replace your code here
-const App = () => <h1>Hello World</h1>
+const App = () => <Projects />
 
 export default App
